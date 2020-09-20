8113b7ef068b3c364a89b080329ca6fa62e8a754
bofengding <d.flippeddd@gmail.com>
2020-09-20 13:06:51 -0400

    initial Project

---
+++ src/main/java/com/example/audiotranscriptiontranslation/AudioTranscriptionTranslationApplication.java
@@ -0,0 +1,13 @@
+package com.example.audiotranscriptiontranslation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTranscriptionTranslationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioTranscriptionTranslationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/audiotranscriptiontranslation/AudioTranscriptionTranslationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audiotranscriptiontranslation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscriptionTranslationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
